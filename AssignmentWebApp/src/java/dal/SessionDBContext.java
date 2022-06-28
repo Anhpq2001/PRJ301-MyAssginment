c696b705252e962ada8d275853d44f09409959ed
Anhpq2001 <106795680+Anhpq2001@users.noreply.github.com>
2022-06-29 04:38:19 +0700

    update schedule page

--- AssignmentWebApp/src/java/dal/SessionDBContext.java
+++ AssignmentWebApp/src/java/dal/SessionDBContext.java
@@ -4,10 +4,175 @@
  */
 package dal;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import Model.Course;
+import Model.Group;
+import Model.Instructor;
+import Model.Room;
+import Model.Session;
+import Model.Timeslot;
+
 /**
  *
  * @author Admin
  */
-public class SessionDBContext {
-    
+public class SessionDBContext extends DBContext<Session>{
+    public ArrayList<Session> getListSessionInSpecificWeek(Date startWeek, Date endWeek) {
+        ArrayList<Session> sessions = new ArrayList<>();
+        try {
+            String sql = "Select ts.TimeslotID,ts.[start],ts.[end],se.SessionID,se.SessionNumber,se.SessionDate,se.Semester,se.RoomID,g.GroupID,g.GroupName,g.InstructorID,c.CourseID,c.CourseName from Timeslot ts \n"
+                    + "join [Session] se On ts.TimeslotID = se.TimeslotID \n"
+                    + "join [Group] g On se.GroupID = g.GroupID\n"
+                    + "join Course c On g.CourseID = c.CourseID\n"
+                    + "Where SessionDate Between ? And ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setDate(1, startWeek);
+            stm.setDate(2, endWeek);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Session se = new Session();
+                se.setSessionID(rs.getInt("SessionID"));
+                se.setSessionNumber(rs.getInt("SessionNumber"));
+                se.setSessionDate(rs.getDate("SessionDate"));
+                se.setSemester(rs.getString("Semester"));
+                Room r = new Room();
+                r.setRoomID(rs.getString("RoomID"));
+                se.setRoomID(r);
+                Group g = new Group();
+                g.setGroupID(rs.getInt("GroupID"));
+                g.setGroupName(rs.getString("GroupName"));
+                Instructor i = new Instructor();
+                i.setInstructorID(rs.getString("InstructorID"));
+                g.setInstructorID(i);
+                Course c = new Course();
+                c.setCourseID(rs.getString("CourseID"));
+                c.setCourseName(rs.getString("CourseName"));
+                g.setCourseID(c);
+                se.setGroupID(g);
+                Timeslot ts = new Timeslot();
+                ts.setTimeslotID(rs.getString("TimeslotID"));
+                ts.setStart(rs.getTime("start"));
+                ts.setEnd(rs.getTime("end"));
+                se.setTimeslotID(ts);
+                sessions.add(se);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(TimeslotDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return sessions;
+    }
+
+    public ArrayList<Session> getSessionInADay(String instructorID, Date date) {
+        ArrayList<Session> sessions = new ArrayList<>();
+        try {
+            String sql = "Select se.SessionID,se.SessionNumber,se.SessionDate,se.Semester,r.RoomID,t.TimeslotID,g.GroupID,t.[start],t.[end],g.GroupName,i.InstructorID,c.CourseID,c.CourseName,i.InstructorName\n"
+                    + "From \n"
+                    + "[Session] se join Timeslot t on se.TimeslotID = t.TimeslotID\n"
+                    + "join Room r on se.RoomID = r.RoomID \n"
+                    + "join [Group] g on se.GroupID = g.GroupID\n"
+                    + "join Course c on g.CourseID =  c.CourseID\n"
+                    + "join Instructor i on g.InstructorID = i.InstructorID\n"
+                    + "Where i.InstructorID = ? AND se.SessionDate = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, instructorID);
+            stm.setDate(2, date);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Session se = new Session();
+                se.setSessionID(rs.getInt("SessionID"));
+                se.setSessionNumber(rs.getInt("SessionNumber"));
+                se.setSessionDate(rs.getDate("SessionDate"));
+                se.setSemester(rs.getString("Semester"));
+                Room r = new Room();
+                r.setRoomID(rs.getString("RoomID"));
+                se.setRoomID(r);
+                Group g = new Group();
+                g.setGroupID(rs.getInt("GroupID"));
+                g.setGroupName(rs.getString("GroupName"));
+                Instructor i = new Instructor();
+                i.setInstructorID(rs.getString("InstructorID"));
+                g.setInstructorID(i);
+                Course c = new Course();
+                c.setCourseID(rs.getString("CourseID"));
+                c.setCourseName(rs.getString("CourseName"));
+                g.setCourseID(c);
+                se.setGroupID(g);
+                Timeslot ts = new Timeslot();
+                ts.setTimeslotID(rs.getString("TimeslotID"));
+                ts.setStart(rs.getTime("start"));
+                ts.setEnd(rs.getTime("end"));
+                se.setTimeslotID(ts);
+                sessions.add(se);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return sessions;
+    }
+
+    public ArrayList<Session> listSessionInAGroup(int groupID) {
+        ArrayList<Session> sessions = new ArrayList<>();
+        try {
+            String sql = "SELECT SessionID,SessionNumber,SessionDate,Semester,RoomID,TimeslotID,GroupID \n"
+                    + "FROM [Session] \n"
+                    + "Where GroupID = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, groupID);
+            ResultSet rs = stm.executeQuery();
+            while(rs.next()){
+                Session se = new Session();
+                se.setSessionID(rs.getInt("SessionID"));
+                se.setSessionNumber(rs.getInt("SessionNumber"));
+                se.setSessionDate(rs.getDate("SessionDate"));
+                se.setSemester(rs.getString("Semester"));
+                Room r = new Room();
+                r.setRoomID(rs.getString("RoomID"));
+                se.setRoomID(r);
+                Timeslot t = new Timeslot();
+                t.setTimeslotID(rs.getString("TimeslotID"));
+                se.setTimeslotID(t);
+                Group g = new Group();
+                g.setGroupID(groupID);
+                se.setGroupID(g);
+                sessions.add(se);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return sessions;
+    }
+
+
+    @Override
+    public ArrayList<Session> list() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Session get(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void insert(Session model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(Session model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(Session model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+   
 }
--- AssignmentWebApp/web/View/schedule/schedule.jsp
+++ AssignmentWebApp/web/View/schedule/schedule.jsp
@@ -9,9 +9,68 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Schedule</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <div class="container">
+            <div>Instructor: A</div>
+            <div >
+                <table class="table table-bordered text-center">
+                    <thead class="thead-light">
+                        <tr>
+                            <th rowspan="1" >Time
+                                <select>
+                                    <option>05/06/2022 - 11/06/2022</option>
+                                    <option>12/06/2022 - 18/06/2022</option><!-- comment -->
+                                    <option>19/06/2022 - 25/06/2022</option><!-- comment -->
+                                    <option>26/06/2022 - 2/07/2022</option><!-- comment -->
+                                    
+                                </select>
+                            </th>
+                            <th>Mon</th>
+                            <th>Tue</th>
+                            <th>Wed</th>
+                            <th>Thu</th>
+                            <th>Fri</th>
+                            <th>Sat</th>
+                            <th>Sun</th>
+
+                        </tr>
+                        <tr>
+                            <c:forEach items="${requestScope.datesinweek}" var="d">
+                                <th>${d}</th>
+                                </c:forEach>
+                        </tr>
+                    </thead>
+                    <tbody >
+                        <c:forEach items="${requestScope.timeslots}" var="t">     
+                            <tr>   
+                                <td>${t.timeslotID}</td>
+                                <c:forEach items="${requestScope.datesinweek}" var="d" >
+                                    <c:set var = "flag" value = "false"/>
+                                    <c:forEach items="${requestScope.sessions}" var="se">
+
+                                        <c:if test="${(se.timeslotID.timeslotID eq t.timeslotID) and (se.sessionDate eq d)}">
+                                            <c:set var = "flag" value = "true"/>
+                                            <td>
+                                                <span>${se.groupID.courseID.courseName}</span>
+                                                <div>at 
+                                                    <span>${se.roomID.roomID}</span>
+                                                </div>
+                                                <div>${se.timeslotID.start}-${se.timeslotID.end}</div>
+                                            </td>
+                                        </c:if>
+                                    </c:forEach>
+                                    <c:if test="${not flag}">
+                                        <td></td>
+                                    </c:if>
+                                </c:forEach>
+                            </tr>
+                        </c:forEach>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </body>
 </html>
