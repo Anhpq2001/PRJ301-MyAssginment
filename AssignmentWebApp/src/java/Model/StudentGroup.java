5fa5adfa4da5ea3292f0f53063ed4a939187940f
Anhpq2001 <106795680+Anhpq2001@users.noreply.github.com>
2022-06-29 02:11:51 +0700

    create models

---
+++ AssignmentWebApp/src/java/Model/StudentGroup.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Admin
+ */
+public class StudentGroup {
+    private Student StudentID;
+    private Group GroupID;
+
+    public Student getStudentID() {
+        return StudentID;
+    }
+
+    public void setStudentID(Student StudentID) {
+        this.StudentID = StudentID;
+    }
+
+    public Group getGroupID() {
+        return GroupID;
+    }
+
+    public void setGroupID(Group GroupID) {
+        this.GroupID = GroupID;
+    }
+}
---
+++ AssignmentWebApp/src/java/Model/Timeslot.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.sql.Time;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Admin
+ */
+public class Timeslot {
+    private String TimeslotID;
+    private Time start;
+    private Time end;
+    private ArrayList<Session> sessions = new ArrayList<>();
+
+    public ArrayList<Session> getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(ArrayList<Session> sessions) {
+        this.sessions = sessions;
+    }
+
+    public String getTimeslotID() {
+        return TimeslotID;
+    }
+
+    public void setTimeslotID(String TimeslotID) {
+        this.TimeslotID = TimeslotID;
+    }
+
+    public Time getStart() {
+        return start;
+    }
+
+    public void setStart(Time start) {
+        this.start = start;
+    }
+
+    public Time getEnd() {
+        return end;
+    }
+
+    public void setEnd(Time end) {
+        this.end = end;
+    }
+
+}
